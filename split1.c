32fec62a3db05d8e8161402b4d7e0258a12e0b18
rishabh <31166087+dthinkcs@users.noreply.github.com>
2018-03-31 13:45:27 +0530

    Create split1.c

---
+++ split1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+
+int main() {
+  char* s =  get_string();
+  
+  
+  char words[10][10];
+  int j = 0, k = 0, space_before = true;
+  for (int i = 0, len = strlen(s); i < len; i++)
+  {
+    if (s[i] != ' ') 
+      words[j][k++] = s[i]
+    else
+    {
+        if (k != 0) 
+          words[j][k] = '\0';
+        k = 0;
+        j++;
+    }
+    
+  }
+  if (k != 0)
+    words[j][k] = '\0';
+    
+    
+  // removing duplicates  
+  for (int i = 0; i < 10; i++)
+  {
+    char* word = words[i];
+    for (int j = i + 1; j < 10; j++)
+      if (strcmp(word, words[j]) == 0)
+        words[j][0] = '\0';
+    
+  }
+  
+  // printing without the duplicates
+  for (int i = 0; i < 10; i++)
+  {
+    if (strcmp(words[i], "") != 0)
+      printf("%s ", words[i]);
+  }
+  
+}
