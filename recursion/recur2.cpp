4d26182582f361c7a88bd60fde552d654c5073a4
r <31166087+dthinkcs@users.noreply.github.com>
2018-11-05 16:22:43 +0530

    Create recur2.cpp

---
+++ recursion/recur2.cpp
@@ -0,0 +1,15 @@
+int power(int x, int n) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+	if (x == 0 && n == 0)
+      return 1;
+  	if (x == 0)
+      return 0;
+  
+	if (n == 0)
+      return 1;
+	return x * power(x, n - 1);
+}
