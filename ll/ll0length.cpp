bb0232a933a84782ccbc822f0bb87fa7687986b2
r <31166087+dthinkcs@users.noreply.github.com>
2018-11-06 14:12:10 +0530

    Create ll0length.cpp

---
+++ ll/ll0length.cpp
@@ -0,0 +1,27 @@
+/**********
+ * Following is the Node class that is already written.
+ 
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+ 
+*********/
+
+int length(Node *head) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input is handled automatically.
+     */
+  int count;
+  Node* ptr;
+  for (count = 0, ptr = head; ptr; count++, ptr=ptr->next);
+  return count;
+    
+}
