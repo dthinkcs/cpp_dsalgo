2806999ce0f00f5f597f00c5442c1f4fbd7ac79b
r <31166087+dthinkcs@users.noreply.github.com>
2018-11-08 12:32:56 +0530

    Create ll12merge.cpp

---
+++ ll/ll12merge.cpp
@@ -0,0 +1,52 @@
+Node* mergeTwoLLs(Node *head1, Node *head2) {
+  /*
+  if(head1==NULL && head2==NULL)
+    return NULL;
+  
+  Node* fh = NULL;
+  Node* ft = NULL;
+
+  if(head1->data <= head2->data)
+  {
+    fh = head1;
+    ft = head1;
+    head1 = head1->next;
+    
+  }
+  
+  else
+  {
+    fh = head2;
+    ft = head2;
+    head2 = head2->next;
+  }
+  
+  while(head1!= NULL && head2!=NULL)
+  {
+ 	if(head1->data >= head2->data)
+	  {
+  	 	ft->next= head2;
+      	head2=head2->next;
+      	ft=ft->next;
+      }
+    else
+      { 
+    	ft->next = head1;
+      	head1=head1->next;
+      	ft=ft->next;
+      }
+  }
+ 
+  if(head1!=NULL)
+  {
+    ft->next = head1;
+  }
+  
+  else
+    ft->next = head2;
+  
+  return fh;
+     */
+     
+     
+}
