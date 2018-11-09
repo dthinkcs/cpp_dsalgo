cd610fa319d302d68a02ddcdb761e64ef6b5da76
r <31166087+dthinkcs@users.noreply.github.com>
2018-11-10 00:10:42 +0530

    Create _BST_v.cpp

---
+++ trees/_BST_v.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+class BSTNode 
+{
+  public:
+  int data;
+  BSTNode* left;
+  BSTNode* right;
+  BSTNode(int d)
+  {
+    data = d;
+    left = right = NULL;
+  }
+  
+};
+
+BSTNode* insertBST(BSTNode* root, int item)
+{
+  if (root == NULL)
+  {
+    root = new BSTNode(item);
+    return root;
+  }
+  
+  if (item < root->data)
+    root->left = insertBST(root->left, item);
+  else 
+    root->right = insertBST(root->right, item);
+  
+  return root;
+}
+
+// do test cases here
+BSTNode* deleteBST(BSTNode* root, int item)
+{
+  
+  BSTNode * ptr = NULL,  * curr = NULL, * par = NULL, * succ = NULL;
+  for( ptr = root; ptr; )
+  {
+    if (ptr->data == item)
+    {
+      curr = ptr;
+      break;
+    }
+    par = ptr;//  curr; //ptr;
+    if (item < ptr->data)
+      ptr = ptr->left;
+    else
+      ptr = ptr->right;
+  }
+  
+  if (curr == NULL)
+    return root;
+    
+  if (curr->left && curr->right)
+  {
+    par = curr;
+    succ = curr->right; 
+    for ( ; succ->left; succ=succ->left)
+    {
+      par = succ;
+    }
+    curr->data = succ->data; // overwrite curr->data with succ->data
+    
+    curr = succ; // change curr to succ
+    
+    
+  }
+  
+  if (par)
+  {
+    if (!curr->left && !curr->right) 
+      if (par->left == curr)
+        par->left = NULL;
+      else 
+        par->right = NULL;
+
+    
+    if (curr->left && !curr->right) 
+      if (par->left == curr)
+        par->left = curr->left;
+      else 
+        par->right = curr->left;
+
+    
+    if (!curr->left && curr->right) 
+      if (par->left == curr)
+        par->left = curr->right;
+      else 
+        par->right = curr->right;
+
+  }
+  
+  delete curr;
+  return root;
+}
+
+int main()
+{
+  BSTNode* root = NULL;
+  root = insertBST(root, 50);
+  insertBST(root, 40);
+  insertBST(root, 60);
+  insertBST(root, 45);
+  insertBST(root, 35);
+  
+  deleteBST(root, 45);
+}
